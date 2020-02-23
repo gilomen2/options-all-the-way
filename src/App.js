1669e75d58f522566d15906d511c90f9f00ce8fa
Beth Gilomen <beth.gilomen@favordelivery.com>
2020-02-22 21:54:49 -0600

    working demo showing code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React, { useState } from 'react'
-import './App.css'
-import { item } from './sample-data'
+import React, { useEffect, useState } from 'react'
+import './App.scss'
+import { items } from './sample-data'
 import { getDefaultSelectedItemOptions, pickSelectionsForOptionGroup } from './helpers'
+import classNames from 'classnames'
+import Expand from 'react-expand-animated'
 
 export const App = () => {
+  const [openItem, setOpenItem] = useState(null)
+  const [currentCodeSnippet, setCurrentCodeSnippet] = useState(null)
+
+  const toggleExpanded = itemId => {
+    setOpenItem(itemId)
+  }
+
   return (
     <div className='App'>
-      <MenuItem item={item} />
+      <div className='menu'>
+        {items.map(item => <MenuItem
+          expandItem={toggleExpanded}
+          setCurrentCodeSnippet={setCurrentCodeSnippet}
+          expanded={openItem === item.id}
+          key={item.id}
+          item={item} />)}
+      </div>
+      {currentCodeSnippet && <div className='code-snippet'>
+        <code>
+          <pre>{JSON.stringify(currentCodeSnippet, null, 2)}</pre>
+        </code>
+      </div>}
     </div>
   )
 }
 
-const MenuItem = ({ item }) => {
+const MenuItem = ({ item, expanded, expandItem, setCurrentCodeSnippet }) => {
   const [currentSelections, setCurrentSelections] = useState(getDefaultSelectedItemOptions(item.option_groups))
+
+  useEffect(() => {
+    if (expanded) {
+      setCurrentCodeSnippet(currentSelections)
+    }
+  }, [expanded, currentSelections])
 
   const handleOptionSelect = selectedOptions => {
     const newSelectedOptions = currentSelections.map(opt => {
@@ -25,19 +52,22 @@ const MenuItem = ({ item }) => {
   }
 
   return (
-    <>
+    <div onClick={() => expandItem(item.id)}>
       <span className='item-name'>{item.name}</span>
-      {item.option_groups.map(optionGroup => <OptionGroup
-        key={optionGroup.id}
-        onChange={handleOptionSelect}
-        handleSelect={handleOptionSelect}
-        optionGroupSelections={pickSelectionsForOptionGroup(optionGroup, currentSelections)}
-        optionGroup={optionGroup} />)}
-    </>
+      <Expand open={expanded}>
+        {item.option_groups.map(optionGroup => <OptionGroup
+          nested={false}
+          key={optionGroup.id}
+          onChange={handleOptionSelect}
+          handleSelect={handleOptionSelect}
+          optionGroupSelections={pickSelectionsForOptionGroup(optionGroup, currentSelections)}
+          optionGroup={optionGroup} />)}
+      </Expand>
+    </div>
   )
 }
 
-const OptionGroup = ({ optionGroup, optionGroupSelections, onChange }) => {
+const OptionGroup = ({ optionGroup, optionGroupSelections, onChange, nested = true }) => {
   const {
     options: optionGroupSelectedOptions = []
   } = optionGroupSelections
@@ -75,7 +105,8 @@ const OptionGroup = ({ optionGroup, optionGroupSelections, onChange }) => {
   }
 
   return (
-    <div className='option-group'>
+
+    <div className={classNames('option-group', nested && 'nested')}>
       <div className='option-group-name'>{optionGroup.name}</div>
       <div className='options'>
         <OptionsList optionGroup={optionGroup}
@@ -85,35 +116,57 @@ const OptionGroup = ({ optionGroup, optionGroupSelections, onChange }) => {
         />
       </div>
     </div>
+
   )
 }
 
 const OptionsList = ({ optionGroup, selectedOptions, onChange, onNestedChange }) => {
   const isRadio = optionGroup.min_selectable === 1 && optionGroup.max_selectable === 1
+
   return (
     <>
       {optionGroup.options.map(option => {
         const isSelected = !!selectedOptions.find(opt => option.id === opt.id)
         return (
-          <div key={option.id}>
-            <input type={isRadio ? 'radio' : 'checkbox'}
-              name={optionGroup.id}
-              id={option.id}
-              value={option.id}
-              onChange={e => onChange(e, option)}
-              checked={isSelected} />
-            <label htmlFor={option.id}>{option.name}</label>
-            {isSelected && option.option_groups.map(og => {
-              const selOpts = selectedOptions.find(sel => sel.id === option.id)
-              return (<OptionGroup
-                key={og.id}
-                onChange={onNestedChange(option.id, selectedOptions)}
-                optionGroupSelections={pickSelectionsForOptionGroup(og, selOpts.option_groups)}
-                optionGroup={og} />)
-            })}
+          <div key={option.id} className='option'>
+            <div className={classNames('pretty p-smooth', isRadio && 'p-round p-default', !isRadio && 'p-svg')}>
+              <input type={isRadio ? 'radio' : 'checkbox'}
+                name={optionGroup.id}
+                id={option.id}
+                value={option.id}
+                onChange={e => onChange(e, option)}
+                checked={isSelected} />
+              <div className='state p-default'>
+                {!isRadio && <Check />}
+                <label className={classNames(isSelected && 'selected')} htmlFor={option.id}>{option.name}</label>
+              </div>
+            </div>
+
+            <Expand open={isSelected}>
+              <div>
+                {option.option_groups.map(og => {
+                  const selOpts = selectedOptions.find(sel => sel.id === option.id)
+                  return (<OptionGroup
+                    key={og.id}
+                    onChange={onNestedChange(option.id, selectedOptions)}
+                    optionGroupSelections={selOpts ? pickSelectionsForOptionGroup(og, selOpts.option_groups) : []}
+                    optionGroup={og}/>)
+                })}
+              </div>
+            </Expand>
           </div>
         )
       })}
     </>
   )
+}
+
+const Check = () => {
+  return (
+    <svg className="svg svg-icon" viewBox="0 0 20 20">
+      <path
+        d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z"
+        style={{ stroke: '#48E5C2', fill: '#48E5C2' }}>
+      </path>
+    </svg>)
 }
